de2ac2b0900b0350575c7981b44d144e409983a0
bbenzie42 <benziebrett@gmail.com>
2024-03-12 12:49:27 -0700

    added updated main file (oops)

--- main.c
+++ main.c
@@ -2,12 +2,42 @@
 #include <stdlib.h>
 int BLOCKSIZE = 512;
 int NUM_TEST_DISKS = 2;
+int TOTAL_DISKS = 0;
+
+/* This functions opens a regular UNIX file and designates the first nBytes of it as space for the emulated disk. 
+If nBytes is not exactly a multiple of BLOCKSIZE then the disk size will be the closest multiple
+of BLOCKSIZE that is lower than nByte (but greater than 0) 
+If nBytes > BLOCKSIZE and there is already a file by the given filename, that file’s content may be overwritten. 
+If nBytes is 0, an existing disk is opened, and the content must not be overwritten in this function. 
+There is no requirement to maintain integrity of any file content beyond nBytes. 
+The return value is negative on failure or a disk number on success. */
 
 int openDisk(char *filename, int nBytes) {
-
+    if(nBytes == 0) {
+        return TOTAL_DISKS;
+    }
+    if(nBytes < BLOCKSIZE) {
+        return -1;
+    }
+    FILE *diskFile = fopen(filename, "wr");
+    int blockOffset = nBytes % BLOCKSIZE;
+    if(blockOffset != 0) {
+        nBytes = nBytes/BLOCKSIZE; //this is incorrect arithmetic i think...
+    }
+    if(nBytes > BLOCKSIZE && diskFile != NULL) {
+        return TOTAL_DISKS; //eventually replaced with disk ID in the superblock given by diskFile
+    }
 }
 
 int closeDisk(int disk) {
+
+}
+
+int readBlock(int disk, int bNum, void* block) {
+
+}
+
+int writeBlock(int disk, int bNum, void* block) {
 
 }
 
@@ -16,7 +46,7 @@ int main()
     int index = 0;
     char *diskName = "fillerdisk.dsk";
     char *buffer;
-    for(index = 0; index < NUM_TEST_DISKS; i++) {
+    for(index = 0; index < NUM_TEST_DISKS; index++) {
         buffer = malloc(BLOCKSIZE * sizeof(char));
     }
     printf("Hello world!\n");
