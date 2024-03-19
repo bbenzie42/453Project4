185f29cdccf5aabdeb323721fdcad1efec183336
pjs507 <paulsoetaert@gmail.com>
2024-03-18 18:56:07 -0700

    organized files to work with given makefile

---
+++ tinyFS.h
@@ -0,0 +1,14 @@
+//
+// Created by pauls on 3/18/2024.
+//
+
+#ifndef INC_453PROJECT4_TINYFS_H
+#define INC_453PROJECT4_TINYFS_H
+
+#define DEFAULT_DISK_SIZE 10240
+#define DEFAULT_DISK_NAME “tinyFSDisk”
+typedef int fileDescriptor;
+
+
+
+#endif //INC_453PROJECT4_TINYFS_H
